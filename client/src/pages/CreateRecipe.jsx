56efe883028ab82313302dd8ed87b86b16460887
Faithvorite <57932015+Faithvorite@users.noreply.github.com>
2023-03-27 17:18:03 -0500

    front end tailwind

---
+++ client/src/pages/CreateRecipe.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CreateRecipe = () => {
+  return (
+    <div className='text-6xl text-red-500'>createRecipe</div>
+  )
+}
+
+export default CreateRecipe
---
+++ client/src/pages/SavedRecipes.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SavedRecipes = () => {
+  return (
+    <div>S</div>
+  )
+}
+
+export default SavedRecipes
